ef900833291946349bb75606db4e638c9d0e5838
Tiago Calderaro <tiagoocalderaro@hotmail.com>
2020-04-17 14:07:54 -0300

    tamo ai mais uma vez

--- calculo.js
+++ calculo.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
-var circulo = require('./circunferencia.js');
+var circunferencia = require('./circunferencia.js');
 
 app.get("/",function(req,res){
-    res.send("Um cilíndro com raio 2 e altura 4 possui volume igual a "+circunferencia.volume(2,4)+" e área externa igual a "+circunferencia.area(2,4)+".");
+    return res.send("Um cilíndro com raio 2 e altura 4 possui volume igual a "+circunferencia.volume(2,4)+" e área externa igual a "+circunferencia.area(2,4)+".");
 })
-app.listen(8182);
+app.listen(process.env.PORT || 8182);
